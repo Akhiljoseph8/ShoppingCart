004cd9551d4a67e3cb0a05f5d23075812099ee8e
Akhil Joseph <akhiljosephaj85@gmail.com>
2025-01-08 17:57:47 +0530

    updated

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb+srv://akhil:akhil@cluster0.kzna9t8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    const url='mongodb+srv://akhil:akhil@cluster0.kzna9t8.mongodb.net/SHOPPINGCART?retryWrites=true&w=majority'
     const dbname='shopping'
    
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
